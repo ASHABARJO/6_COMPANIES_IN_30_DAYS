5c37bb10ab0a52e2ba0a579b406205f8fa528352
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-01-26 21:27:38 +0530

    Create strictly_palidromic_num.cpp

---
+++ GOOGLE_DAY5/strictly_palidromic_num.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+
+Intitution:-
+4=100(Base) so 4 is strictly palidromic number for n>4 consider base n-2.
+In base n-1,n=11.
+In base n-2,n=12, so n is not strictly palidromic number.
+There is no strictly palidromic number n where n>=4.
