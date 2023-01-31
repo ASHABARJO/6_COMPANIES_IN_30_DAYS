86b2ed87dcd54823fdb1004936d677f70366c757
ASHA BARJO <89405319+ASHABARJO@users.noreply.github.com>
2023-01-31 20:13:12 +0530

    Create max_length.cpp

---
+++ AMAZON_DAY4/max_length.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int ans=0;
+        int n1=nums1.size(),n2=nums2.size();
+        vector<int>dp(n1+1,0);
+        for(int i=0;i<n1;i++){
+            for(int j=n2-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                dp[j+1]=1+dp[j];
+            }
+            else{
+            dp[j+1]=0;
+        }
+        ans=max(ans,dp[j+1]);
+        }
+    }
+        
+    return ans;
+    }
+};
